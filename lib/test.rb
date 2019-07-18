56b3a29528e6394a52bc5b625db134e71e7812b2
Takumikat <Takumikat@users.noreply.github.com>
2019-07-18 23:25:09 +0200

    test Githubdesktop

---
+++ lib/test.rb
@@ -0,0 +1,10 @@
+def sum_of_3_or_5_multiples(final_number)
+
+def is_multiple_of_3_or_5?(current_number)	#indique si true ou false
+	n=0
+	n +=1
+	while n<=1000
+	if n%3 ==0 
+	puts "true"
+end
+end
